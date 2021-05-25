4fe88617f53dd241f11f2ee6ba1e30bff2d1f531
MrAlves17 <vitor_alves_barbosa@hotmail.com>
2021-05-24 23:41:56 -0300

    usuario quase completo

---
+++ src/Professor.java
@@ -0,0 +1,54 @@
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+public class Professor extends Usuario implements Observador {
+
+	private int qtdNotificacoes;
+
+	public Professor(String codigoUsuario, String nomeUsuario) {
+		super(codigoUsuario, nomeUsuario);
+	}
+	
+	@Override
+	public void atualizar() {
+		this.qtdNotificacoes++;		
+	}
+
+	@Override
+	public boolean verificarRestricoesEmprestimo(String codigoLivro) {
+		boolean usuarioDevedor = isUsuarioDevedor();
+		
+		if(!usuarioDevedor) {
+			System.out.println("O usuário está apto para realizar um empréstimo");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isUsuarioDevedor() {
+		for(int i=0; i<getEmprestimosCorrentes().size(); i++) {
+			Emprestimo e = getEmprestimosCorrentes().get(i);
+			long diasEntreHojeEmprestimo = ChronoUnit.DAYS.between(Calendar.getInstance().toInstant(),e.getDataEmprestimo().toInstant());
+			if(diasEntreHojeEmprestimo > 7) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean verificarPrioridade() {
+		return true;
+	}
+	
+	@Override
+	public int numeroDiasEmprestimo() {
+		return 7;
+	}
+	
+	@Override
+	public int getQtdNotificacoes() {
+		return this.qtdNotificacoes;
+	}
+	
+}
---
+++ src/Reserva.java
@@ -0,0 +1,44 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Reserva {
+	private Livro livro;
+	private Usuario usuario;
+	private Calendar dataReserva;
+	
+	public Reserva(Livro livro, Usuario usuario) {
+		this.livro = livro;
+		this.usuario = usuario;
+		this.dataReserva = Calendar.getInstance();
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public Calendar getDataReserva() {
+		return dataReserva;
+	}
+	
+	public String getDataReservaEmString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		return formatter.format(getDataReserva().getTime());
+	}
+	
+	public String toString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		
+		String emprestimoCorrente = String.format(""
+				+ "Titulo: %s\n"
+				+ "Data da Reserva: %s\n",
+				getLivro().getTitulo(),
+				getDataReservaEmString());
+		
+		return emprestimoCorrente;
+	}
+
+}
