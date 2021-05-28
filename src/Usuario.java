72f95a394d270561a96281955cf954f7859cb6bc
MrAlves17 <vitor_alves_barbosa@hotmail.com>
2021-05-28 13:35:24 -0300

    corrigindo problema diferença entre os dias de empréstimo e devolução

--- src/Usuario.java
+++ src/Usuario.java
@@ -53,8 +53,7 @@ public abstract class Usuario implements Observador{
 	public boolean isUsuarioDevedor() {
 		for(int i=0; i<getEmprestimosCorrentes().size(); i++) {
 			Emprestimo e = getEmprestimosCorrentes().get(i);
-			long diasEntreHojeEmprestimo = ChronoUnit.DAYS.between(Calendar.getInstance().toInstant(),e.getDataEmprestimo().toInstant());
-			System.out.println("dias entre hoje Emprestimo -> "+diasEntreHojeEmprestimo);
+			long diasEntreHojeEmprestimo = ChronoUnit.DAYS.between(e.getDataEmprestimo().toInstant(),Calendar.getInstance().toInstant());
 			if(diasEntreHojeEmprestimo > numeroDiasEmprestimo()) {
 				return true;
 			}
